5f29bd358fc12b35666e35ad57e7a860abc766d7
javi sanchez <javier.san.mail@gmail.com>
2024-06-07 13:00:45 +0200

    fix set diferents solutions at solutionCode

--- src/components/codeSolution.tsx
+++ src/components/codeSolution.tsx
@@ -15,21 +15,28 @@ export default function CodeSolution({ allCodeSolutions }) {
 	useEffect(() => {
 		setCodeLanguage(data.codeLanguage);
 		setSolutionCode(allCodeSolutions[data.codeLanguage]);
-
-		const solutionCode = allCodeSolutions[data.codeLanguage];
-		async function transform() {
-			const result = await codeToHtml(solutionCode[0], {
-				lang: data.codeLanguage,
-				theme: data.theme === "dark" ? "dark-plus" : "light-plus",
-				transformers: [transformerNotationHighlight(), transformerNotationDiff()],
-			});
-			setHtml(result);
-		}
-		void transform();
+		updateCode();
 	}, [data.codeLanguage, data.theme]);
 
+	const updateCode = async (index = 0) => {
+		const solutionCode = allCodeSolutions[data.codeLanguage];
+		console.log;
+		const result = await codeToHtml(solutionCode[index], {
+			lang: data.codeLanguage,
+			theme: data.theme === "dark" ? "dark-plus" : "light-plus",
+			transformers: [transformerNotationHighlight(), transformerNotationDiff()],
+		});
+		setHtml(result);
+	};
+
+	const tabHandler = (val) => {
+		const indexString = val.match(/\d+/);
+		const index = Number(indexString[0]) - 1;
+		updateCode(index);
+	};
+
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col h-[100%]">
 			{/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
 			<div
 				className="ml-6 mt-4 w-[0px] flex-1"
@@ -37,10 +44,10 @@ export default function CodeSolution({ allCodeSolutions }) {
 				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
 				dangerouslySetInnerHTML={{ __html: html }}
 			/>
-			<div className={"sticky bottom-0 w-full flex-1"}>
-				<div className={"border-t w-full flex-1 px-6 py-2"}>
+			<div className={"sticky bottom-0 w-full bg-white dark:bg-black"}>
+				<div className={"border-t border-neutral-300 dark:border-neutral-800 w-full flex-1 px-6 py-2"}>
 					<div className="flex w-full flex-col">
-						<Tabs aria-label="Options" variant="light">
+						<Tabs aria-label="Options" variant="light" onSelectionChange={(val) => tabHandler(val)}>
 							{solutionCode.map((solution: string, i: number) => {
 								return <Tab key={`Solution ${i + 1}`} title={`Solution ${i + 1}`} />;
 							})}
--- src/components/description.tsx
+++ src/components/description.tsx
@@ -8,18 +8,11 @@ export default function DescriptionComponent({ challenge }) {
 	const [index, setIndex] = useState<number>(0);
 
 	return (
-		<div className="border-2 border-neutral-200 dark:border-neutral-800 rounded-xl overflow-hidden bg-white dark:bg-transparent">
-			<div className="relative h-[100%] overflow-scroll">
-				<TabsDescription
-					onTabChange={(num: number) => {
-						setIndex(num);
-						return {};
-					}}
-				/>
-				{index === 0 && <ChallengeDescription challenge={challenge} />}
-				{index === 1 && <TestCases testCases={challenge.testCases} />}
-				{index === 2 && <CodeSolution allCodeSolutions={challenge.solutionCode} />}
-			</div>
+		<div className="overflow-scroll flex flex-col border-2 border-neutral-200 dark:border-neutral-800 rounded-xl bg-white dark:bg-transparent">
+			<TabsDescription onTabChange={(num: number) => setIndex(num)} />
+			{index === 0 && <ChallengeDescription challenge={challenge} />}
+			{index === 1 && <TestCases testCases={challenge.testCases} />}
+			{index === 2 && <CodeSolution allCodeSolutions={challenge.solutionCode} />}
 		</div>
 	);
 }
