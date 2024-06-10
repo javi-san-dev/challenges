3ec76ccb942b0b7e24b7284c3c9cb3ee549503e2
javi sanchez <javier.san.mail@gmail.com>
2024-06-10 17:42:54 +0200

    add auth config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
-
 import auth from "auth-astro";
 
 // https://astro.build/config
