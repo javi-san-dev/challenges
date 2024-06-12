81b2594b4e7f20df4dc34fbd5c2df4b975e7937c
javi sanchez <javier.san.mail@gmail.com>
2024-06-12 16:51:22 +0200

    remove test code and add getSession function

--- src/pages/api/getEnvVars.json.ts
+++
@@ -1,8 +0,0 @@
-
-
-export function GET(context) {
-	// const AUTH_SECRET = context.env.AUTH_SECRET;
-	const AUTH_SECRET = import.meta.env.AUTH_SECRET
-	console.log("GETTINg ENV VARS", AUTH_SECRET)
-	return new Response(AUTH_SECRET);
-}
