cde187c576d6a5174db5f4fdc513f761abe9a312
javi sanchez <javier.san.mail@gmail.com>
2024-06-14 15:34:23 +0200

    replace cloudflare adapter with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	output: "server",
-	adapter: cloudflare(),
-	vite: {
-		ssr: {
-			external: ["node:path"],
-		},
-	},
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
